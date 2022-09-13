eb8883488b12fa2efa2a5de4320e6571373d4145
Haris0408 <Harishpal0408@gmail.com>
2022-09-13 23:38:19 +0530

    creating a palindrome programe need your help

---
+++ problem1.js
@@ -0,0 +1,13 @@
+ let str1;
+   let str2;
+   for(i=0; i<=str.length-1;i++){
+       str1=str[i];
+   }
+   for(let i=str.length-1;i>=0;--i){
+       str2=str[i];
+   }
+   if(str1==str2){
+       console.log("Yes");}
+       else{
+           console.log("Yes");
+       }
